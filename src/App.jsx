332a165a9bde8402f6b78a0d1f55e96d42f573ba
chief <mahdirashidi462@gmail.com>
2023-09-17 22:18:29 +0330

    create new test project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      {/* <form>
+        <input type="text" name='' />
+        <input type="text" name='' />
+        <button>
+          submit
+        </button>
+      </form> */}
+    </>
+  );
+}
+
+export default App;

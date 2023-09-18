dbde7bf883a6312876c8dfc010bfbcad5a879ff5
chief <mahdirashidi462@gmail.com>
2023-09-18 22:50:52 +0330

    fix add new test the addNewUser functionality

--- src/tests/userForm.test.js
+++ src/tests/userForm.test.js
@@ -1,10 +1,25 @@
-import { render,screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
+import user from "@testing-library/user-event"
 import UserForm from '../components/userForm'
 
-test('this is the first test i write',()=>{
-render(<UserForm/>)
-const inputs=screen.getAllByRole('textbox')
-const button=screen.getByRole('button')
-expect(inputs).toHaveLength(2)
-expect(button).toBeInTheDocument()
+test('this is the first test i write', () => {
+    render(<UserForm />)
+    const inputs = screen.getAllByRole('textbox')
+    const submitBtn = screen.getByRole('button')
+    expect(inputs).toHaveLength(2)
+    expect(submitBtn).toBeInTheDocument()
+})
+test('this test show that user can type into the inputs', () => {
+    const mock = jest.fn()
+    render(<UserForm addNewUser={mock} />)
+    const [nameInput, emailInput] = screen.getAllByRole('textbox')
+    const submitBtn = screen.getByRole('button')
+    user.click(nameInput)
+    user.keyboard('hello')
+    user.click(emailInput)
+    user.keyboard('hello@hello.com')
+
+    user.click(submitBtn)
+    expect(mock).toHaveBeenCalled()
+    expect(mock).toHaveBeenCalledWith({name:'hello',email:'hello@hello.com'})
 })
